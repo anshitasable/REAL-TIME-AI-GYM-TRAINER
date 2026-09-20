ed911253c5b25b94cc1c415fb6c133f1b7f9fd80
Anshita Sable <anshitasable25@gmail.com>
2026-09-20 10:53:39 +0530

    Database Setup (sqlite)

--- services/auth/login_wall.py
+++ services/auth/login_wall.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from services.persistence.exercise_repository import get_or_create_user
 
 def render_login_wall():
     if st.session_state.get("user_id")is not None:
@@ -13,8 +14,10 @@ def render_login_wall():
         if not username:
             st.error("Name cannot be empty")
             return False
-        st.session_state["username"]=username
-        st.session_state["user_id"]="1"
+        user = get_or_create_user(username)
+        
+        st.session_state["username"]=user["username"]
+        st.session_state["user_id"]=user["id"]
         st.rerun()
 
     return False
